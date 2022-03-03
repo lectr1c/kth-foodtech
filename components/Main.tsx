51fed1665611a1a63a6df1a5d399c0eed26d4453
lectr1c <66423346+lectr1c@users.noreply.github.com>
2022-03-03 05:15:19 +0100

    Chakra UI implementation and tests

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,17 +1,17 @@
 import styled from "styled-components";
-
-// @ts-ignore
+import {Box, flexbox, Grid, GridItem} from "@chakra-ui/react";
 
 interface mainProps {
     readonly green?: boolean;
 }
+
 const MainContainer = styled.main<mainProps>`
   height: 95vh;
   width: 100%;
-  background-color: ${props => props.green ? "lightgreen" : "lightgray"};
+  background-color: ${props => props.green ? "gray" : "lightgray"};
   
   :hover {
-    background-color: ${props => props.green ? "lightgray" : "lightgreen"};
+    background-color: ${props => props.green ? "lightgray" : "gray"};
   }
   
   @media screen and (min-height: 1440px){
@@ -19,11 +19,26 @@ const MainContainer = styled.main<mainProps>`
   }
 `
 
+const data = {
+    "data":[
+        {"title":"Box 1"},
+        {"title":"Box 2"},
+        {"title":"Box 3"}
+    ]
+}
+
 const Main = (props: object) => {
     return (
-        <MainContainer green>
-
-        </MainContainer>
+        <Box backgroundColor={"gray.500"} height='100vh' width='100%'>
+            <Grid gap={1} templateRows='repeat(2, 1fr)' templateColumns='repeat(3, 1fr)' pt={20} mx={20}>
+                {data.data.map((item) => {
+                    return <GridItem w='100%' h='100' bg='green.600' key={item.title}>{item.title}</GridItem>;
+                })}
+                <GridItem colSpan={2} bg='purple.700'/>
+                <GridItem colSpan={1} bg='green.300'/>
+                <GridItem rowStart={0} rowEnd={2} colStart={0} colEnd={0} rowSpan={2} colSpan={1} bg='red.300'> Hi </GridItem>
+            </Grid>
+        </Box>
     )
 }
 
