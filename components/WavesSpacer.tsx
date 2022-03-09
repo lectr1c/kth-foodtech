d559c6ccaf0fee075151d55619c9d8d7af316a21
lectr1c <66423346+lectr1c@users.noreply.github.com>
2022-03-09 05:52:19 +0100

    Waves fix

--- components/WavesSpacer.tsx
+++ components/WavesSpacer.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import waves from "../public/waves.svg";
-import wave from "../public/wave.svg";
 
 const WavesSpacer = () => {
     return (
         <div>
             <Image src={waves} alt={"waves"} layout={"responsive"}/>
-            <Image src={wave} alt={"waves"} layout={"responsive"}/>
         </div>
     )
 }
