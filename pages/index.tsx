d453432f0fd026f3a3417acacbc2e3b7f19068ec
lectr1c <66423346+lectr1c@users.noreply.github.com>
2022-03-12 01:11:45 +0100

    IOS Fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,14 @@
 import type { NextPage } from 'next'
-import Navigation from "../components/Navigation";
-import Hero from "../components/Hero";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "../components/Footer";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import WavesSpacer from "../components/WavesSpacer";
 import Main from "../components/Main";
 import SchemeToggler from "../components/SchemeToggler";
-import {Divider} from "@mantine/core";
+import Navigation from "../components/Navigation";
 
 const Home: NextPage = () => {
   return (
     <div>
-        {/*<Navigation/>*/}
+        <Navigation/>
         <Main/>
         <Footer/>
         <SchemeToggler/>
