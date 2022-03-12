5e4f6f90328c9146943f50833c6d926590a71f70
lectr1c <66423346+lectr1c@users.noreply.github.com>
2022-03-12 01:20:09 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "../components/Footer";
 import Main from "../components/Main";
 import SchemeToggler from "../components/SchemeToggler";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from "../components/Navigation";
 
 const Home: NextPage = () => {
