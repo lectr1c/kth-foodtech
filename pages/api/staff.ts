ca5898399431509a7057c67b8a4ff7118b7d35bd
lectr1c <66423346+lectr1c@users.noreply.github.com>
2022-09-19 15:25:24 +0200

    Update staff.ts

--- pages/api/staff.ts
+++ pages/api/staff.ts
@@ -44,7 +44,6 @@ export default async function handler(
 
       if (req.method == "GET") {
           if (req.body.id){
-              console.log("hhh")
 
           } else {
               staffRepo.getStaff()
